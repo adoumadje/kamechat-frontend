8d51c0cb507e54d0347c9c3215cb7d74a7693e07
adoumadje <adoumadjecaleb@gmail.com>
2025-01-12 15:19:17 +0100

    list of users

--- src/app/components/main-page/main-page.component.ts
+++ src/app/components/main-page/main-page.component.ts
@@ -1,9 +1,10 @@
+import { NgFor, NgIf } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-main-page',
   standalone: true,
-  imports: [],
+  imports: [NgFor, NgIf],
   templateUrl: './main-page.component.html',
   styleUrl: './main-page.component.css'
 })
